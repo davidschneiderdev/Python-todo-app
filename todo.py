ab86e9d4feb32501929dcfab05b45be862a53608
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-13 15:24:08 -0500

    add print todo function

--- todo.py
+++ todo.py
@@ -4,17 +4,23 @@
 # keep track of todos using a list
 todo_list = []
 
+# print my todos
+def print_todos():
+    for count, todo in enumerate(todo_list, 1):
+        print(f"{count}. {todo}")
+
 # way to add todos 
 def add_todo(todo):
     # We receive a todo which is a string
     todo_list.append(todo)
 
 # print the empty todo list
-print(todo_list)
+
 # add a todo by calling our function
 add_todo("feed the cat")
+add_todo("take out trash")
+add_todo("pay bill")
 # make sure it was added
-print(todo_list)
 
 # need to be able to delete todos
 def delete_todo(index):
@@ -23,14 +29,30 @@ def delete_todo(index):
     except IndexError:
         print("Sorry, no todo at that index.")
 
-
-# delete_todo(0)
-
-print(todo_list)
-# print my todos
-for count, todo in enumerate(todo_list, 1):
-    print(f"{count}. {todo}")
-    
 # show user the main menu
+def show_menu():
+    print("            ")
+    print("   Todo List")
+    print("================")
+    print("0. quit")
+    print("1. print todos")
+    print("2. add a todo")
+    print("3. complete a todo")
+
+status = True
+while status:
+    show_menu()
+    user_choice = int(input("Choose one: "))
+    if user_choice == 0:
+        status = False
+    elif user_choice == 1:
+        print_todos()
+    elif user_choice == 2:
+        todo_prompt = input("Enter todo: ")
+        
+
+    
+
+
 
 
