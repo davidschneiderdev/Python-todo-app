0fe37de21635e94e90ec26a286a446a5d649db5a
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-13 15:59:18 -0500

    adds delete feature

--- todo.py
+++ todo.py
@@ -6,20 +6,21 @@ todo_list = []
 
 # print my todos
 def print_todos():
-    for count, todo in enumerate(todo_list, 1):
-        print(f"{count}. {todo}")
+    if len(todo_list) == 0:
+        print("You have nothing to do!")
+    else:
+        for count, todo in enumerate(todo_list):
+            print(f"{count}: {todo}")
 
 # way to add todos 
-def add_todo(todo):
+def add_todo(todo_prompt):
     # We receive a todo which is a string
-    todo_list.append(todo)
+    todo_list.append(todo_prompt)
 
 # print the empty todo list
 
 # add a todo by calling our function
-add_todo("feed the cat")
-add_todo("take out trash")
-add_todo("pay bill")
+
 # make sure it was added
 
 # need to be able to delete todos
@@ -30,26 +31,39 @@ def delete_todo(index):
         print("Sorry, no todo at that index.")
 
 # show user the main menu
-def show_menu():
-    print("            ")
-    print("   Todo List")
-    print("================")
-    print("0. quit")
-    print("1. print todos")
-    print("2. add a todo")
-    print("3. complete a todo")
+def main():
+    menu = """
+                    
+    Todo List
+================
+0. quit
+1. print todos
+2. add a todo
+3. complete a todo
+                    
+"""
+    is_running = True
+    while is_running:
+        print(menu)
+        choice = input("Choose one: ")
+        if choice == "0":
+            is_running = False
+            print("Thank you for using the todo app.")
+        elif choice == "1":
+            print_todos()
+        elif choice == "2":
+            todo_prompt = input("Enter todo: ")
+            add_todo(todo_prompt)
+        elif choice == "3":
+            index_to_complete = int(input("Complete which todo? "))
+            delete_todo(index_to_complete)
+        else:
+            print("Please enter a number for your choice.")
 
-status = True
-while status:
-    show_menu()
-    user_choice = int(input("Choose one: "))
-    if user_choice == 0:
-        status = False
-    elif user_choice == 1:
-        print_todos()
-    elif user_choice == 2:
-        todo_prompt = input("Enter todo: ")
-        
+main()
+
+
+
 
     
 
